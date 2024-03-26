eca3e08cda3d07bf4bd71096b92b24cad8c441ca
Harendra singh <94151837+Hasheditz@users.noreply.github.com>
2024-03-26 22:32:15 +0530

    New

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@styles/global.css';
+
+export const metadata = {
+    title : "promptshare",
+    description : 'Discover & share prompt.'
+}
+
+const layout = () => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'></div>
+            </div>
+        </body>
+    </html>
+  )
+}
+
+export default layout
