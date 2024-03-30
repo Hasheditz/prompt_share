f8cd3b54fda69f70398d6021593bd186c1c2c479
Harendra singh <94151837+Hasheditz@users.noreply.github.com>
2024-03-30 22:59:28 +0530

    SignIn

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const home = () => {
     <section className= "w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share 
-        <br className="max-md : hidden" />
+        <br/>
         <span className="orange_gradient text-center"> AI - Powered Prompts</span>
       </h1>
       <p className="desc text-center">Here is the best AI Prompts generating and sharing app where you can share and find the best prompts for the further work. Specially designed for the chat-GPT , Devin , Mid Journey</p>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,23 +1,61 @@
-"use-client";
+'use-client'
 
 import Link from 'next/link';
 import Image from 'next/image';
 import { UseState , UseEffect } from 'react';
-import {signIn , signOut, useSession , getProviders} from 'next-auth/react';
+import {signIn , signOut , useSession , getProviders} from 'next-auth/react';
 
 const Nav = () => {
 
+  const IsUser = true;
+
+  return(
+
   <nav className="flex-between w-full mb-16 pt-3">
-    <link href = '/' className='flex gap-2 flex-center'>
+
+    <Link href = '/' className="flex gap-2 flex-center">
         <Image
-        src='assets\images\logo.svg'
+        src='/assets/images/logo.svg'
         alt='My logo'
         width={30}
         height={30}
         className='object-contain'
-        />    
-    </link>
+        />
+
+        <p className='logo_text'> Prompt-Share </p>
+
+
+    </Link> 
+
+    {/* Desktop navigation */}
+
+    <div className='sm:flex hidden'>
+      {IsUser ? (
+      <div className='flex gap-3 md:gap-5'>
+        <Link href = "/create-prompt" className = 'black_btn'>
+          create prompt
+        </Link>
+        
+        {/* <button type='button' onClick={signOut} className='outline_btn'>
+        Sign Out 
+        </button> */}
+        
+        <Link href='/profile'>
+          <Image
+          src='/assets/images/logo.svg'
+          alt='Profile'
+          className='rounded-full'
+          width={37}
+          height={37}
+          />
+        </Link>
+      </div> 
+      ) : (
+        <>  </>
+      )}
+    </div>
   </nav>
+  )
 }
 
 export default Nav
